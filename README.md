7edae3287b9cfd65ad8be1cf8e13acfc3f815bb9
Gaurav Chaturvedi <gaurav@Gauravs-MacBook-Air.local>
2024-05-08 18:43:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# WeatherUnion
+
+WeatherUnion is a sample project that utilizes data from [WeatherUnion](https://www.weatherunion.com) to retrieve weather information.
+
+## Usage
+
+To use this Python code, you need to follow these steps:
+
+1. Register with [WeatherUnion](https://www.weatherunion.com) and obtain an API Key.
+2. Modify the `config.json` file and add your API Key there.
+3. Obtain the location ID from the PDF file (`locations.pdf`) from this [This repository](https://github.com/oddtazz/weatherunion/blob/main/locations.pdf). Select the appropriate localityId and add it to the `config.json` file.
+
+## Getting Started
+
+1. Clone this repository.
+2. `pip install requests`
+3. Modify the `config.json` file with your API Key and locality ID.
+4. Run the Python script.
+
+## Example Config.json
+
+```json
+{
+ "location": "ZWL000000",
+ "api_key": "your_api_key_here"
+}
+```
+
+## Sample output
+```~$ python weather.py
+Temprature: 22.72 Degrees C
+Humidity: 83.32%
+Wind Speed: 0.84 km/h
+Wind Direction: 289.2 Degrees
+Rain Intensity: 0.2 mm/min
+Rain Accumulation: 6.8 mm/h
+```
+
+## Compatibility
+
+This code has been tested with Python 3.12.3.
---
+++ weather.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+def get_weather_data(location, api_key):
+    url = f"https://www.weatherunion.com/gw/weather/external/v0/get_locality_weather_data?locality_id={location}"
+    headers = {"x-zomato-api-key": api_key}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        print(f"Request failed with status code {response.status_code}")
+        return None
+
+# Read API key and location from config.json
+with open('config.json') as f:
+    config = json.load(f)
+    location = config['location']
+    api_key = config['api_key']
+
+weather_data = get_weather_data(location, api_key)
+if weather_data:
+    locality_weather_data = weather_data.get("locality_weather_data")
+    if locality_weather_data:
+        humidity = locality_weather_data.get("humidity")
+        wind_speed = locality_weather_data.get("wind_speed")
+        temperature = locality_weather_data.get("temperature")
+        wind_direction = locality_weather_data.get("wind_direction")
+        rain_intensity = locality_weather_data.get("rain_intensity")
+        rain_accumulation = locality_weather_data.get("rain_accumulation")
+        print(f"Temperature: {temperature} Degrees C")
+        print(f"Humidity: {humidity}%")
+        print(f"Wind Speed: {wind_speed} km/h")
+        print(f"Wind Direction: {wind_direction} Degrees")
+        print(f"Rain Intensity: {rain_intensity} mm/min")
+        print(f"Rain Accumulation: {rain_accumulation} mm/h")
